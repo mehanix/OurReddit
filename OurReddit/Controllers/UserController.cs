568885c22c98ee8092d0f1da385b7a06449807fb
Stefan Radu <stefanr@stonesouptech.com>
2020-10-28 00:18:21 +0200

    We did parte din tema

---
+++ OurReddit/Controllers/UserController.cs
@@ -0,0 +1,88 @@
+﻿using OurReddit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OurReddit.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserDBContext db = new UserDBContext();
+
+        [HttpGet]
+        public String Index()
+        {
+            var users = from user in db.Users
+                        orderby user.Name
+                        select user;
+
+            ViewBag.Users = users;
+            return "yes";
+        }
+        
+        [HttpGet]
+        public ActionResult Show(int id)
+        {
+            User user = db.Users.Find(id);
+            ViewBag.User = user;
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult New(User user)
+        {
+            try
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            User user = db.Users.Find(id);
+            ViewBag.User = user;
+            var users = from usr in db.Users select usr;
+            return View();
+        }
+
+        [HttpPut]
+        public ActionResult Edit(int id, User requestUser)
+        {
+            try
+            {
+                User user = db.Users.Find(id);
+                if (TryUpdateModel(user))
+                {
+                    user.Name = requestUser.Name;
+                    user.Email = requestUser.Email;
+                    user.Password = requestUser.Password;
+                    user.Birthday = requestUser.Birthday;
+                    user.Description = requestUser.Description;
+                    user.RoleID = requestUser.RoleID;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            return View();
+        }
+    }
+}
---
+++ OurReddit/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OurReddit.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; } // hased :D
+        public string Description { get; set; }
+        public DateTime Birthday { get; set; }
+        public int RoleID { get; set; }
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext() : base("DBConnectionString") { }
+        public DbSet<User> Users { get; set; }
+    }
+}
